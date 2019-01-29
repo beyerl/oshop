92c0aa860721c1e33c4d176295e556c09fe22854
Lorenz Beyer <lorenzbeyer@googlemail.com>
2019-01-29 15:26:38 +0100

    implement navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule, Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ShoppingCartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot([
+      { 
+        path: '',
+        component: HomeComponent 
+      },
+      { 
+        path: 'shopping-cart', 
+        component: ShoppingCartComponent 
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
