f6aaa37bca07c9ea31779a8ed2f694b02b35dc43
Lorenz Beyer <lorenzbeyer@googlemail.com>
2019-01-31 17:29:51 +0100

    add further changes by mosh

--- src/app/guards/admin-auth.guard.ts
+++ src/app/guards/admin-auth.guard.ts
@@ -1,7 +1,11 @@
+import { DbService } from './../services/db.service';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { map, switchMap } from 'rxjs/operators';
+import * as firebase from 'firebase';
+import { AppUser } from '../models/app-user';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +14,35 @@ export class AdminAuthGuard implements CanActivate {
 
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private db: DbService
   ){  }
 
   canActivate() {
-    console.log("isAdmin: " + this.authService.userData.isAdmin); 
+    return this.authService.moshUser$.pipe(
+      switchMap(user => this.db.get(user.uid).valueChanges()),
+      map(appUser => appUser.isAdmin)
+    )
+  }
+
+  /*console.log(this.authService.afAuth.auth.currentUser)
+    let uid = this.authService.afAuth.auth.currentUser.uid;
+    return this.db.get(uid)
+    .valueChanges()
+    .pipe(map((appUser: AppUser) => appUser.isAdmin));*/
+
+  /*canActivate(): Observable<boolean> {
+    return this.authService.moshUser$
+      .pipe(
+        switchMap(user => {return this.db.get(user.uid).valueChanges()}
+        )).pipe*/
+    /*console.log("isAdmin: " + this.authService.userData.isAdmin); 
     if (this.authService.userData.isAdmin) {
       return true;
     }
     else {
       this.router.navigate(['']);
       return false
-    }
-  }
+    }*/
+  //}
 }
